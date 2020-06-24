c016c239b44fb4d8d67eb740c46229812cc4fe10
Mason <masontyler747@gmail.com>
2020-06-23 20:34:53 -0400

    Added tip calculating functionality to calculator

--- Tip Calculator/ViewController.m
+++ Tip Calculator/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 @end
 
@@ -22,6 +26,23 @@
 - (IBAction)onTap:(id)sender {
     //Function that is called whenever the tap gesture is performed
     NSLog(@"Hello");
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    //Function that is called whenever the edit status of the text field changes or the value of the Tip Control changes
+    double bill = [self.billField.text doubleValue];
+    
+    NSArray *percentages = @[@(0.15), @(0.2), @(0.22)]; //Create an NSArray of NS numbers
+    
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat: @"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat: @"$%.2f", total];
+    
 }
 
 @end
